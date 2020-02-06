d92bedf130ecf5fed3558f52b4c3f19b6a89ac79
Alex_GD12 <alexandrecracco@gmail.com>
2020-02-06 14:14:25 +0100

    Update tp_exploration.c

--- tp_exploration.c
+++ tp_exploration.c
@@ -16,9 +16,16 @@ int main () {
 	int  destination = 0;
 	int tamponDestination = 0;
 	
+	
+	//choix destination
 	printf ("Bonjour aventurier, vous souhaitez allez dans le desert du nord (1) ? ou dans les plaines du sud (2) ?\n");
 	
 	scanf("%d", &destination);
+	
+	while(destination != 1 && destination != 2){
+		printf("Eh la ! tu me prends pour un taxi salopiaud ? J'vais pas la bas moi!\n");
+		scanf("%d", &destination);
+	}
 	
 	if( destination == 1){
 		printf("Tres bien, montez a l'arriere et nous partirons.\n Apres un long chemin dans la cariole, vous ne voyez plus que du sable a perte d'horizon, du sable ? Non des cendres... \n");
@@ -28,10 +35,7 @@ int main () {
 		printf("Tres bien, montez a l'arriere et nous partirons.\n Apres un long chemin dans la cariole, vous sentez l'air frais sur votre visage, le paysage est vert, il ya de nombreux chevaux sauvages... quelques bouquetins des plaines egalement.\n");
 	}
 
-	while(destination != 1 && destination != 2){
-		printf("Eh la ! tu me prends pour un taxi salopiaud ? J'vais pas la bas moi!");
 	
-	}
 
 
 
