57a1f1ab188f156bf8ed8147caab0cda664cfbd8
Alex_GD12 <alexandrecracco@gmail.com>
2020-02-06 14:38:04 +0100

    Update tp_exploration.c

--- tp_exploration.c
+++ tp_exploration.c
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-=======
+	
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,31 +15,31 @@ int main () {
 	
 	//variables
 	char destination [30];
-	char destination1 [30] = "Sud";
-	char destination2 [30] = "Nord";
+	char destination1 [30] = "Sud\0";
+	char destination2 [30] = "Nord\0";
 	
 	
 	//choix destination
-	printf ("Bonjour aventurier, vous souhaitez allez dans le desert du nord (1) ? ou dans les plaines du sud (2) ?\n");
+	printf ("Bonjour aventurier, vous souhaitez allez dans le desert du nord ? ou dans les plaines du sud ?\n");
 	
-	scanf("%d", &destination);
+	scanf("%s", &destination);
 	
-	while(destination1 != 0 && destination2 != 0){
+	while (strcmp(destination, destination1) != 0 && strcmp (destination, destination2) != 0){
 		printf("Eh la ! tu me prends pour un taxi salopiaud ? J'vais pas la bas moi!\n");
-		scanf("%d", &destination);
+		scanf("%s", &destination);
 	}
 	
-	if( destination == 1){
+	if(strcmp( destination,  destination1) == 0){
 		printf("Tres bien, montez a l'arriere et nous partirons.\n Apres un long chemin dans la cariole, vous ne voyez plus que du sable a perte d'horizon, du sable ? Non des cendres... \n");
 	}
 
-	if( destination == 2){
+	if(strcmp( destination,  destination2) == 0){
 		printf("Tres bien, montez a l'arriere et nous partirons.\n Apres un long chemin dans la cariole, vous sentez l'air frais sur votre visage, le paysage est vert, il ya de nombreux chevaux sauvages... quelques bouquetins des plaines egalement.\n");
 	}
 
 	
 
 
-
+	return 0;
 }
->>>>>>> Stashed changes
+
